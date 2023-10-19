d520a0c11c7a855fcd9555855f39ce15c4e2c15f
xsaxsaxsa <13384189+xsaxsaxsa@users.noreply.github.com>
2023-10-19 12:52:50 +0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_DRD_sprout.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_DRD_sprout-user \
+    twrp_DRD_sprout-userdebug \
+    twrp_DRD_sprout-eng
